1d994b8da368de740ce11a91dc99c25345a6a08c
Lucas wilman da silva crispim <Lucas Wilman da Silva Crispim>
2023-10-13 17:56:09 -0300

    eigth

--- stockindicators/views.py
+++ stockindicators/views.py
@@ -2,9 +2,21 @@ from django.shortcuts import render
 from .forms import StockRangeForm
 import yfinance as yf
 import math
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
+
+    html = f'''
+    <html>
+        <body>
+            <h1>Hello from Vercel!</h1>
+            <p>The current time is { now }.</p>
+        </body>
+    </html>
+    '''
+    return HttpResponse(html)
+
 
     if request.method == 'POST':
 
