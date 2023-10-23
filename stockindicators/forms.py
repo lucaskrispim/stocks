4ea9826ffb41322e305e3b501b58a8f703b1cd71
Lucas wilman da silva crispim <Lucas Wilman da Silva Crispim>
2023-10-22 22:26:14 -0300

    add indicators

--- stockindicators/forms.py
+++ stockindicators/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class StockRangeForm(forms.Form):
-    nome = forms.CharField(label="Sigla Ação",max_length=100)
-    data_inicial = forms.DateField(input_formats=['%Y-%m-%d'], widget=forms.DateInput(attrs={'type': 'date'}))
-    data_final = forms.DateField(input_formats=['%Y-%m-%d'], widget=forms.DateInput(attrs={'type': 'date'}))
+    nome = forms.CharField(label="Sigla Ação",max_length=100,required=True,error_messages={'required': 'Por favor, insira uma sigla de uma ação brasileira.'})
+    data_inicial = forms.DateField(input_formats=['%Y-%m-%d'], widget=forms.DateInput(attrs={'type': 'date'}),error_messages={'required': 'Por favor, insira uma data válida.'})
+    data_final = forms.DateField(input_formats=['%Y-%m-%d'], widget=forms.DateInput(attrs={'type': 'date'}),error_messages={'required': 'Por favor, insira uma data válida.'})
--- stockindicators/views.py
+++ stockindicators/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from .forms import StockRangeForm
 import yfinance as yf
+from .indicators import RSI
 import math
 from django.http import HttpResponse
+from datetime import datetime, timedelta
+import numpy as np
 
 # Create your views here.
 def index(request):
@@ -14,13 +17,34 @@ def index(request):
         if form.is_valid():
 
             try:
-                df2 = yf.download(f"{form.data['nome']}.sa",start=f"{form.data['data_inicial']}",end=f"{form.data['data_final']}", progress=False)
-                print(df2)
+                date_obj = datetime.strptime(form.data['data_inicial'], '%Y-%m-%d')
+
+                result_date = date_obj - timedelta(days=100)
+
+                df2 = yf.download(f"{form.data['nome']}.sa",start=f"{result_date.strftime('%Y-%m-%d')}",end=f"{form.data['data_final']}", progress=False)
+                data = []
+                data = RSI(data=df2,column='Close', window=14)
+
+                vol = round(data['Variation'].std(),2)
+
+                avg_return = round(data['Variation'].mean(),2)
+
+                var = round(np.quantile(data['Variation'][:], 0.05),2)
+                data = data.round(2)
+                data.reset_index(level=0, inplace=True)
+                data = data[data['Date'] >= form.data['data_inicial']]
+                data['Date'] = data['Date'].dt.strftime('%d-%m-%Y')
+                data = data.to_dict(orient='records')
+
             except Exception as e:
                 print(f"Erro: {e}")
 
             context = {
-            'form':StockRangeForm
+            'form':StockRangeForm,
+            'companies':data,
+            'vol':vol,
+            'avg_return':avg_return,
+            'var':var
             }
 
             return render(request, 'index.html',context=context)
